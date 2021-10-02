3ad306b72c6e5ee5421fd518d35aa2fec1a4ff99
axsynos <temurqalandarov.me@gmail.com>
2021-10-03 00:58:13 +0500

    fix

--- src/core/server.js
+++ src/core/server.js
@@ -4,7 +4,7 @@ const
   routes = require('../routes'),
   expressLayouts = require('express-ejs-layouts'),
   { TOKEN, ENVIRONMENT, URL, PORT } = require('./config'),
-  { get } = require('https'),
+  // { get } = require('https'),
   { dirname, join } = require('path')
 
 app
@@ -16,12 +16,12 @@ app
 module.exports = async (bot) => {
   await require('./db')()
   if (ENVIRONMENT === 'prod') {
-    bot.telegram.setWebhook(URL + '/bot' + TOKEN)
-    app.use(bot.webhookCallback('/bot' + TOKEN))
-
-    setInterval(() => {
-      get(URL)
-    }, 1500000)
+    // bot.telegram.setWebhook(URL + '/bot' + TOKEN)
+    // app.use(bot.webhookCallback('/bot' + TOKEN))
+    bot.launch().then(() => console.log('App working...')).catch(e => console.log(e))
+    // setInterval(() => {
+    //   get(URL)
+    // }, 1500000)
   }
   else if (ENVIRONMENT === 'dev')
     bot.launch().then(() => console.log('App working...')).catch(e => console.log(e))
